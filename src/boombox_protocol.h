75f927a0bb2b8a2f4d6418f915d3327ed3cbc7f5
Adam Lyon <adam@nullrider.com>
2025-12-12 17:12:33 -0700

    Added global for wifi channel selection

--- src/boombox_protocol.h
+++ src/boombox_protocol.h
@@ -2,32 +2,31 @@
 #include <Arduino.h>
 
 #define MAX_SYSTEM_NAME_LEN 25
+#define WIFI_CHANNEL 3
 
 // Shared enums
 // enums for StatusMsg, I think?
-enum REMOTE_MODE {
+enum REMOTE_MODE
+{
   REMOTE,
   LOCAL
 };
 
 enum ARM_STATUS
 {
-    NOT_ARMED,
-    ARMED
+  NOT_ARMED,
+  ARMED
 };
 
-
-
-
-
-
 // Used by the queue on the master side when sending a message
-enum MasterSendReqType : uint8_t {
+enum MasterSendReqType : uint8_t
+{
   SEND_FIRE_CMD = 1,
   SEND_STATUS_REQ = 2
 };
 
-typedef struct {
+typedef struct
+{
   MasterSendReqType type;
   uint8_t mac[6];
   uint8_t port;
@@ -35,12 +34,14 @@ typedef struct {
 } MasterSendRequest;
 
 // Used by the remote when sending outgoing message
-enum SendReqType : uint8_t {
-    SEND_ACK = 1,
-    SEND_STATUS = 2
+enum SendReqType : uint8_t
+{
+  SEND_ACK = 1,
+  SEND_STATUS = 2
 };
 
-typedef struct {
+typedef struct
+{
   SendReqType type;
 } SendRequest;
 
@@ -48,37 +49,37 @@ typedef struct {
 // BEFORE MsgType IS PARSED AND
 // data[32] GETS CAST TO THE CORRECT MESSAGE TYPE
 // Generic incoming message struct
-typedef struct {
+typedef struct
+{
   uint8_t data[32];
   uint8_t mac[6];
   int len;
 } IncomingMsg;
 
-
 // THESE ARE THE MESSAGES THAT ACTUALLY GET SENT BACK AND FORTH
 
 // Message type definitions
 // Enum to identify the message type
-enum MsgType : uint8_t {
+enum MsgType : uint8_t
+{
   MSG_FIRE = 1,
   MSG_STATUS = 2,
   MSG_STATUS_REQUEST = 3,
   MSG_AWK = 4
 };
 
-
 // From master to remote
-typedef struct __attribute__((packed)) {
-  MsgType type;      // always MSG_FIRE
+typedef struct __attribute__((packed))
+{
+  MsgType type; // always MSG_FIRE
   uint8_t portId;
   uint16_t holdMs;
 } FireMsg;
 
-
-
 // from remote to master
-typedef struct __attribute__((packed)) {
-  MsgType type;      // always MSG_STATUS
+typedef struct __attribute__((packed))
+{
+  MsgType type; // always MSG_STATUS
   uint8_t mode;
   char systemName[MAX_SYSTEM_NAME_LEN];
   uint8_t isArmed;
@@ -87,12 +88,13 @@ typedef struct __attribute__((packed)) {
 } StatusMsg;
 
 // from master to remote
-typedef struct __attribute__((packed)) {
-  MsgType type;      // always MSG_CONFIG
+typedef struct __attribute__((packed))
+{
+  MsgType type; // always MSG_CONFIG
 } StatusRequest;
 
-
 // from remote to master
-typedef struct __attribute__((packed)) {
-  MsgType type;      // always MSG_CONFIG
+typedef struct __attribute__((packed))
+{
+  MsgType type; // always MSG_CONFIG
 } AwkMsg;
